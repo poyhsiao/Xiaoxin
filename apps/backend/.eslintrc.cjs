f7b80dcaa8b473655464ebf3c81382b79af1ebf6
Kim Hsiao <white.shopping@gmail.com>
2026-07-14 18:44:14 +0800

    fix(backend): add ESLint config to resolve CI lint failure

---
+++ apps/backend/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
+  },
+  ignorePatterns: ['dist'],
+};
